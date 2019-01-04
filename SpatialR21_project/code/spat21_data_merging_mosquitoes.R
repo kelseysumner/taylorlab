08b65eafffe45c7b3735af4db841b729198319e9
kelseysumner <sumner.kelsey@gmail.com>
2019-01-04 16:07:42 -0500

    tabulating mosquito data

--- SpatialR21_project/code/spat21_data_merging_mosquitoes.R
+++ SpatialR21_project/code/spat21_data_merging_mosquitoes.R
@@ -48,17 +48,36 @@ unmerged_ids_descriptive = anoph_descriptive_data[-which(anoph_merged_data$sampl
 unmerged_ids_qpcr = anoph_qpcr_data[-which(anoph_qpcr_data$sample_id_mosquito %in% anoph_merged_data$sample_id_mosquito),]
 # 3 mosquito sample IDs didn't merge from the qpcr data
 
+# export the merged data for now
+# write_csv(anoph_merged_data, "spat21_mosquito_anopheles_merged_data_4JAN2019.csv")
+# write_rds(anoph_merged_data, "spat21_mosquito_anopheles_merged_data_4JAN2019.RDS")
+
 # create tabulations of the mosquito data
+# create table 2
 table(anoph_merged_data$abdominal_status, anoph_merged_data$village, useNA = "always")
 table(anoph_merged_data$species_type, anoph_merged_data$village, useNA = "always")
 table(anoph_merged_data$village, useNA = "always")
 average_mosquitoes = anoph_merged_data %>%
-  group_by(village, HH_ID) %>%
-  summarize(total_n = sum(total_num_mosq_in_hh, na.rm = T)) %>%
+  group_by(village,HH_ID,repeat_instance) %>%
+  summarize(total_n = max(total_num_mosq_in_hh, na.rm = T)) %>%
+  group_by(village,HH_ID) %>%
+  summarize(total_n_hh = sum(total_n)) %>%
   group_by(village) %>%
-  summarize(avg_hh = mean(total_n, na.rm = T), lower_bound = min(total_n), upper_bound = max(total_n))
-summary(anoph_merged_data$total_num_mosq_in_hh)
+  summarize(avg_hh = mean(total_n_hh, na.rm = T), lower_bound = min(total_n_hh), upper_bound = max(total_n_hh))
+average_mosquitoes_total = anoph_merged_data %>%
+  group_by(village,HH_ID,repeat_instance) %>%
+  summarize(total_n = max(total_num_mosq_in_hh, na.rm = T)) %>%
+  group_by(village,HH_ID) %>%
+  summarize(total_n_hh = sum(total_n))
+summary(average_mosquitoes_total)
 
+# create table 3
+table3 = table(anoph_merged_data$species_type, anoph_merged_data$abdominal_status, useNA = "always")
+table3 = table3[,1:3]
+table(anoph_merged_data$species_type, anoph_merged_data$hb_status_mosquito_level, useNA = "always")
+table(anoph_merged_data$species_type, anoph_merged_data$pf_infection_status_mosquito_level, useNA = "always")
+table(anoph_merged_data$species_type, anoph_merged_data$pf_pcr_infection_status_sample_level_h, useNA = "always")
+table(anoph_merged_data$species_type, anoph_merged_data$pf_pcr_infection_status_sample_level_a, useNA = "always")
 
 
 
