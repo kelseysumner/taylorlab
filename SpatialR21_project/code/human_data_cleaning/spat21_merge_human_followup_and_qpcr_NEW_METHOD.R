a341c2f7cdf4d5f3c3fcfa382b788f2b6fea44f8
kelseysumner <sumner.kelsey@gmail.com>
2019-04-19 21:44:13 -0400

    cleaning mosquito sequence data for spat21

--- SpatialR21_project/code/human_data_cleaning/spat21_merge_human_followup_and_qpcr_NEW_METHOD.R
+++ SpatialR21_project/code/human_data_cleaning/spat21_merge_human_followup_and_qpcr_NEW_METHOD.R
@@ -49,15 +49,79 @@ length(which(is.na(human_merged_all_data$`Sample Name`) == T)) # 0 missing
 count_table = table(human_merged_all_data$`Sample Name`, useNA = "always")
 dups_table = count_table[which(count_table > 1)] # 0 duplicates
 
+# first remove the columns that are not needed (standards)
+cols_to_remove = c("HbtubStd1a","HbtubStd1b","HbtubStd2a","HbtubStd2b","HbtubStd3a","HbtubStd3b",
+                   "HbtubStd4a","HbtubStd4b","HbtubStd5a","HbtubStd5b","HbtubStd6a","HbtubStd6b",
+                   "HbtubStd7a","HbtubStd7b","HbtubStd8a","HbtubStd8b","HbtubStd9a","HbtubStd9b",
+                   "HbtubStd10a","HbtubStd10b","pfr364Std1a","pfr364Std1b","pfr364Std2a","pfr364Std2b",
+                   "pfr364Std3a","pfr364Std3b","pfr364Std4a","pfr364Std4b","pfr364Std5a","pfr364Std5b",
+                   "pfr364Std6a","pfr364Std6b","pfr364Std7a","pfr364Std7b","pfr364Std8a","pfr364Std8b",
+                   "pfr364Std9a","pfr364Std9b","pfr364Std10a","pfr364Std10b","r_value_std","intercept_std",
+                   "slope_std", "Well Position","HbtubCT1","HbtubCT2","pfr364CT1","pfr364CT2",
+                   "pfr364Q1","pfr364Q2","pfr364Q1_std","pfr364Q2_std","Experiment Name")
+human_qpcr_data = human_qpcr_data[,!(colnames(human_qpcr_data) %in% cols_to_remove)]
+
+# add an empty column for the date associated with the sample id for the social demographic data
+str(human_merged_all_data$today_hum_monthly_data)
+str(human_merged_all_data$today_hum_sick_data)
+human_merged_all_data$sample_id_date = if_else(is.na(human_merged_all_data$today_hum_sick_data),human_merged_all_data$today_hum_monthly_data,human_merged_all_data$today_hum_sick_data)
+table(human_merged_all_data$sample_id_date, useNA = "always")
+str(human_merged_all_data$sample_id_date)
+
+# add an empty column for the date associated with the sample id for the qpcr data
+qpcr_new_date = rep(NA,nrow(human_qpcr_data))
+for (i in 1:nrow(human_qpcr_data)){
+  split_id=strsplit(human_qpcr_data$`Sample Name`[i],"-")[[1]]
+  qpcr_new_date[i] = split_id[2]
+}
+human_qpcr_data$qpcr_new_date = dmy(qpcr_new_date)
+table(human_qpcr_data$qpcr_new_date, useNA="always")
+head(human_qpcr_data$qpcr_new_date)
+head(human_qpcr_data$`Sample Name`)
+
+# create a household id variable for qpcr data
+mem_id = rep(NA,nrow(human_qpcr_data))
+for (i in 1:nrow(human_qpcr_data)){
+  split_id=strsplit(human_qpcr_data$`Sample Name`[i],"-")[[1]]
+  mem_id[i] = paste0(split_id[1],"_",split_id[3])
+}
+human_qpcr_data$mem_id = mem_id
+table(human_qpcr_data$mem_id, useNA="always")
+head(human_qpcr_data$mem_id)
+head(human_qpcr_data$`Sample Name`)
+
+# create a large for loop that will merge together the qpcr results if they match the social demographic data by within 6 years
+# add three empty columns to the human social demographic data set
+human_merged_all_data$sample_name_from_merge = rep(NA,nrow(human_merged_all_data))
+human_merged_all_data$pf_pcr_infection_status = rep(NA,nrow(human_merged_all_data))
+human_merged_all_data$pfr364Q_std_combined = rep(NA,nrow(human_merged_all_data))
+human_merged_all_data = human_merged_all_data[c(1:10,3100:3110),]
+# then do the for loop
+for (i in 1:nrow(human_merged_all_data)){
+  for (j in 1:nrow(human_qpcr_data)){
+    if (human_merged_all_data$`Sample Name`[i]==human_qpcr_data$`Sample Name`[j]){
+      human_merged_all_data[i,130:132] = human_qpcr_data[j,1:3]
+    } else if (str_detect(human_qpcr_data$`Sample Name`[j],"R") & 
+               str_detect(human_merged_all_data$`Sample Name`[i],"R") & 
+               (human_qpcr_data$mem_id[j]==human_merged_all_data$unq_memID[i]) & 
+               (as.numeric(human_qpcr_data$qpcr_new_date[j] - human_merged_all_data$sample_id_date[i])<=6) &
+               (as.numeric(human_qpcr_data$qpcr_new_date[j] - human_merged_all_data$sample_id_date[i])>0)) {
+      human_merged_all_data[i,130:132] = human_qpcr_data[j,1:3]
+    } else if (!(str_detect(human_qpcr_data$`Sample Name`[j],"R")) & 
+               !(str_detect(human_merged_all_data$`Sample Name`[i],"R")) & 
+               (human_qpcr_data$mem_id[j]==human_merged_all_data$unq_memID[i]) & 
+               (as.numeric(human_qpcr_data$qpcr_new_date[j] - human_merged_all_data$sample_id_date[i])<=6) &
+               (as.numeric(human_qpcr_data$qpcr_new_date[j] - human_merged_all_data$sample_id_date[i])>0)){
+      human_merged_all_data[i,130:132] = human_qpcr_data[j,1:3]
+    }
+  }
+}
+small_data = human_merged_all_data[,125:132]
 
 
-
-
-
-
-
-
-
+# extra code in case need to add in
+!(human_qpcr_data$`Sample Name`[j] %in% human_merged_all_data$sample_name_from_merge[i]) & 
+  !(human_merged_all_data$`Sample Name`[i] %in% human_merged_all_data$sample_name_from_merge[i]) & 
 
 
 
---
+++ SpatialR21_project/code/mosquito_data_cleaning/spat21_haplotype_cleaning_mosquitoes.R
@@ -0,0 +1,220 @@
+# ----------------------------------------- #
+#       Spat21 Haplotype Output Cleaning    #
+#               Mosquito Data               #
+#              April 17, 2019               #
+#                K. Sumner                  #
+# ----------------------------------------- #
+
+
+#### --------- load packages ----------------- ####
+library(readr)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(data.table)
+library(tableone)
+library(MHCtools)
+library(dada2)
+
+
+#### ------- read in the AMA haplotype output -------------- ####
+
+# read in the haplotype data set
+foo = read_rds("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_spat21_mosquito_haplotypes.rds")
+
+# figure out how many rows and columns
+nrow(foo)
+ncol(foo)
+
+### --- look at the raw haplotype output 
+
+# remove the controls (BF289 and BF294)
+foo = foo[-which(rownames(foo) == "BF289" | rownames(foo) == "BF294"),]
+
+# summarize the samples for each haplotype
+haplotype.names = rep(1:ncol(foo))
+haplotypes_in_samples = rep(NA,ncol(foo))
+total_reads_in_samples = rep(NA,ncol(foo))
+for (k in 1:ncol(foo)){
+  haplotypes_in_samples[k] = length(which(foo[,k] > 0))
+  total_reads_in_samples[k] = sum(foo[,k],na.rm=T)
+}
+haplotype_num_summary = data.frame("haplotype_ids" = haplotype.names, "haplotypes_across_samples" = haplotypes_in_samples, "total_reads_across_samples" = total_reads_in_samples)
+# censor haplotypes found with <500 reads 
+haplotype_num_summary = haplotype_num_summary[-which(haplotype_num_summary$total_reads_across_samples < 500),]
+# output the haplotype summary
+write_csv(haplotype_num_summary,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_haplotype_num_summary.csv")
+
+# enforce censoring to rds data set
+foo = foo[,c(haplotype_num_summary$haplotype_ids)]
+
+# write some code that summarizes the haplotypes across each sample for censored haplotypes
+sample.names = row.names(foo)
+haplotype_num = rep(NA,nrow(foo))
+haplotype_reads = rep(NA,nrow(foo))
+for (i in 1:nrow(foo)){
+  haplotype_num[i] = length(which(foo[i,] > 0))
+  haplotype_reads[i] = sum(foo[i,])
+}
+haplotype_summary = data.frame("sample_names" = sample.names, "haplotype_number" = haplotype_num, "haplotype_reads" = haplotype_reads)
+# remove samples that ended up with no reads at the end
+needtoremove = which(haplotype_summary$haplotype_reads == 0) # 2 samples removed
+haplotype_summary = haplotype_summary[-needtoremove,]
+# export as a csv
+write_csv(haplotype_summary,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_sample_summary.csv")
+
+# write out the haplotypes results as a fasta
+uniquesToFasta(getUniques(foo), fout="Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_uniqueSeqs.fasta", ids=paste0("Seq", seq(length(getUniques(foo)))))
+
+# rename the columns to be a unique haplotype column number
+newcolnames = c(1:ncol(foo))
+pastedcolnames = rep(NA,length(newcolnames))
+for (i in 1:length(newcolnames)){
+  pastedcolnames[i] = paste0("H",newcolnames[i])
+}
+colnames(foo) <- pastedcolnames
+
+# make the matrix a dataframe
+foo = as.data.frame(foo)
+
+# create a new column of foo that is the sample names (rownames)
+foo$`MiSeq.ID` = rownames(foo)
+colnames(foo)
+
+# censor the rds file
+foo = foo[which(foo$MiSeq.ID %in% haplotype_summary$sample_names),]
+
+# write out the new rds file with censoring enforced
+write_csv(foo,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_spat21_mosquito_haplotypes_censored.csv")
+
+
+# calculate number of SNPs in haplotypes from MESA CSP haplotypes
+library(Biostrings)
+dna = readDNAStringSet("Desktop/Meshnick Lab/Steve Taylor's Lab/Webuye MESA Sequence Data/Haplotype analysis ALL data/haplotype_output/CSP/CSP_uniqueSeqs.fasta")
+snp_output = stringDist(dna, method="hamming")
+snp_output = as.matrix(snp_output)
+max(snp_output)
+summary(snp_output)
+
+
+
+
+#### ------- read in the CSP haplotype output -------------- ####
+
+# read in the haplotype data set
+foo = read_rds("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/spat21_mosquitoes_CSP_haplotypes.rds")
+
+# figure out how many rows and columns
+nrow(foo)
+ncol(foo)
+
+### --- look at the raw haplotype output 
+
+# remove the controls (BF289 and BF294)
+foo = foo[-which(rownames(foo) == "BF289" | rownames(foo) == "BF294"),]
+
+# summarize the samples for each haplotype
+haplotype.names = rep(1:ncol(foo))
+haplotypes_in_samples = rep(NA,ncol(foo))
+total_reads_in_samples = rep(NA,ncol(foo))
+for (k in 1:ncol(foo)){
+  haplotypes_in_samples[k] = length(which(foo[,k] > 0))
+  total_reads_in_samples[k] = sum(foo[,k],na.rm=T)
+}
+haplotype_num_summary = data.frame("haplotype_ids" = haplotype.names, "haplotypes_across_samples" = haplotypes_in_samples, "total_reads_across_samples" = total_reads_in_samples)
+# censor haplotypes found with <500 reads 
+haplotype_num_summary = haplotype_num_summary[-which(haplotype_num_summary$total_reads_across_samples < 500),]
+# output the haplotype summary
+write_csv(haplotype_num_summary,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_haplotype_num_summary.csv")
+
+# enforce censoring to rds data set
+foo = foo[,c(haplotype_num_summary$haplotype_ids)]
+
+# write some code that summarizes the haplotypes across each sample for censored haplotypes
+sample.names = row.names(foo)
+haplotype_num = rep(NA,nrow(foo))
+haplotype_reads = rep(NA,nrow(foo))
+for (i in 1:nrow(foo)){
+  haplotype_num[i] = length(which(foo[i,] > 0))
+  haplotype_reads[i] = sum(foo[i,])
+}
+haplotype_summary = data.frame("sample_names" = sample.names, "haplotype_number" = haplotype_num, "haplotype_reads" = haplotype_reads)
+# remove samples that ended up with no reads at the end
+needtoremove = which(haplotype_summary$haplotype_reads == 0) # 3 samples removed
+haplotype_summary = haplotype_summary[-needtoremove,]
+# export as a csv
+write_csv(haplotype_summary,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_sample_summary.csv")
+
+# write out the haplotypes results as a fasta
+uniquesToFasta(getUniques(foo), fout="Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_uniqueSeqs.fasta", ids=paste0("Seq", seq(length(getUniques(foo)))))
+
+# rename the columns to be a unique haplotype column number
+newcolnames = c(1:ncol(foo))
+pastedcolnames = rep(NA,length(newcolnames))
+for (i in 1:length(newcolnames)){
+  pastedcolnames[i] = paste0("H",newcolnames[i])
+}
+colnames(foo) <- pastedcolnames
+
+# make the matrix a dataframe
+foo = as.data.frame(foo)
+
+# create a new column of foo that is the sample names (rownames)
+foo$`MiSeq.ID` = rownames(foo)
+colnames(foo)
+
+# censor the rds file
+foo = foo[which(foo$MiSeq.ID %in% haplotype_summary$sample_names),]
+
+# write out the new rds file with censoring enforced
+write_csv(foo,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_spat21_mosquito_haplotypes_censored.csv")
+
+
+
+#### --------- read in the anopheles demographic data set -------- ####
+
+# read in the mosquito demographic data (merged anpopheles mosquito data set)
+anoph_merged_data = read_rds("/Users/kelseysumner/Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/clean data/merged_data/spat21_mosquito_anopheles_merged_data_18JAN2019.RDS")
+
+# read in the cleaned ama haplotype data
+ama_haps = read_csv("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_spat21_mosquito_haplotypes_censored.csv")
+
+# read in the cleaned csp haplotype data
+csp_haps = read_csv("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_spat21_mosquito_haplotypes_censored.csv")
+
+# read in the mosquito miseq inventory
+miseq_inventory = read_csv("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/mosquito_sequencing_inventory/spat21_mosquito_sequencing_inventory_with_CT_values.csv")
+
+# cut down the miseq_inventory to just the columns of interest
+miseq_inventory = miseq_inventory[,c(1,2,9,10)]
+miseq_inventory = rename(miseq_inventory, "MiSeq.ID" = "MiSeq ID", "sample_id" = "Sample ID")
+
+# merge in the mosquito miseq inventory with the ama haplotype data
+ama_merge_data = left_join(miseq_inventory,ama_haps,by="MiSeq.ID")
+
+# export the ama_merge_data
+write_csv(ama_merge_data,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_spat21_mosquito_haplotypes_censored_final.csv")
+
+# merge in the mosquito miseq inventory with the csp haplotype data
+csp_merge_data = left_join(miseq_inventory,csp_haps,by="MiSeq.ID")
+
+# export the csp_merge_data
+write_csv(csp_merge_data,"Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_spat21_mosquito_haplotypes_censored_final.csv")
+
+# read in the ama sample summary
+ama_sample_summary = read_csv("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/AMA/AMA_sample_summary.csv")
+
+# read in the csp sample summary
+csp_sample_summary = read_csv("Desktop/Dissertation Materials/SpatialR21 Grant/Final Dissertation Materials/Final Data Sets/Final Cohort data June 2017 to July 2018/Mosquito data/sequence_results/haplotype_results/haplotype_output/CSP/CSP_sample_summary.csv")
+
+
+#### ------------- look at mosquito data summaries ----------- ####
+
+# look at the summaries
+summary(anoph_merged_data)
+nrow(ama_haps)
+ncol(ama_haps)
+summary(ama_sample_summary$haplotype_reads)
+nrow(csp_haps)
+ncol(csp_haps)
+summary(csp_sample_summary$haplotype_reads)
