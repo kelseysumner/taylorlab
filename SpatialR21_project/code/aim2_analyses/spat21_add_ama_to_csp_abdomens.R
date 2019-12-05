7555a687d2a712892b686dd06e50b749d7760c94
kelseysumner <sumner.kelsey@gmail.com>
2019-12-05 13:42:13 -0500

    adding haplotype lists for haplotype sharing

---
+++ SpatialR21_project/code/aim2_analyses/spat21_add_ama_to_csp_abdomens.R
@@ -0,0 +1,49 @@
+# -------------------------------------- #
+#           Spat21/Mozzie Study          #
+# Add AMA haplotype sharing to CSP data  #
+#   for human-mosquito abdomen sharing   #
+#                 Aim 2                  #
+#            Mozzie Phase 1              #
+#               K. Sumner                #
+#          December 3, 2019              #
+# -------------------------------------- #
+
+
+#### -------- load packages ------------ ####
+
+# load in the packages of interest
+library(dplyr)
+library(readr)
+library(tidyr)
+
+
+
+#### ----- read in the data sets ----- ####
+
+# read in the csp data set for mosquito abdomens
+csp_data = read_rds("Desktop/clean_ids_haplotype_results/CSP/csp_edgelists_no_restrictions/spat21_csp_edgelist_abdomen_3DEC2019.rds")
+
+# read in the ama data set for mosquito heads
+ama_data = read_rds("Desktop/clean_ids_haplotype_results/AMA/ama_edgelists_no_restrictions/spat21_ama_edgelist_abdomen_no_restrictions_3DEC2019.rds")
+ama_data = ama_data %>%
+  select(-X1)
+
+
+#### ----- create variable distributions ------ ####
+
+# note: Focus of analysis will be on csp
+
+## create a distribution based on if >= 1 haplotype is shared between that pair in ama as well as csp
+
+# first create a variable that indicates whether there's at least 1 haplotype shared in ama as well (is number of haplotypes shared in ama)
+csp_data$haps_shared_in_ama = rep(NA,nrow(csp_data))
+for (i in 1:nrow(csp_data)){
+  if (csp_data$haps_shared[i] > 0) {
+    for (j in 1:nrow(ama_data)){
+      if (csp_data$sample_id_human[i] == ama_data$sample_id_human[j] & csp_data$sample_id_abdomen[i] == ama_data$sample_id_abdomen[j] & ama_data$haps_shared[j] > 0){
+        csp_data$haps_shared_in_ama[i] = ama_data$haps_shared[j]
+      }
+    }
+  }
+}
+
--- SpatialR21_project/code/aim2_analyses/spat21_build_computational_model.R
+++ SpatialR21_project/code/aim2_analyses/spat21_build_computational_model.R
@@ -20,35 +20,29 @@ library(tidyr)
 #### ----- read in the data sets ----- ####
 
 # read in the csp data set for mosquito abdomens
-csp_data = read_rds("Desktop/clean_ids_haplotype_results/CSP/csp_edgelists_no_restrictions/spat21_csp_edgelist_abdomen_3DEC2019.rds")
-
-# read in the ama data set for mosquito heads
-ama_data = read_rds("Desktop/clean_ids_haplotype_results/AMA/ama_edgelists_no_restrictions/spat21_ama_edgelist_abdomen_no_restrictions_3DEC2019.rds")
-ama_data = ama_data %>%
-  select(-X1)
+csp_data = read_rds("Desktop/clean_ids_haplotype_results/CSP/csp_edgelists_no_restrictions/csp_abdomens_with_ama/csp_edgelist_abdomens_with_ama.rds")
 
 
 #### ----- create variable distributions ------ ####
 
 # note: Focus of analysis will be on csp
 
-## create a distribution based on if >= 1 haplotype is shared between that pair in ama as well as csp
+## create a distribution based on the distance between each human-mosquito pair
 
-# first create a variable that indicates whether there's at least 1 haplotype shared in ama as well (is number of haplotypes shared in ama)
-csp_data$haps_shared_in_ama = rep(NA,nrow(csp_data))
-for (i in 1:nrow(csp_data)){
-  if (csp_data$haps_shared[i] > 0) {
-    for (j in 1:nrow(ama_data)){
-      if (csp_data$sample_id_human[i] == ama_data$sample_id_human[j] & csp_data$sample_id_abdomen[i] == ama_data$sample_id_abdomen[j] & ama_data$haps_shared[j] > 0){
-        csp_data$haps_shared_in_ama[i] = ama_data$haps_shared[j]
-      }
-    }
-  }
-}
+# create dummy datasets
+pairs = c("pair1", "pair2", "pair3")
+hap = c("H1","H2","H3")
+prev = c(0.5,0.2,0.15)
+haps_shared = c(1,2,3)
+order_haps = c("H1","H1,H2","H1,H2,H3")
+pair_dummy_df = data.frame(pairs,haps_shared,order_haps)
+hap_dummy_df = data.frame(hap,prev)
+pair_dummy_df
+hap_dummy_df
 
-
-
-
-
+1-(1-.5)
+1-(1-.5)*(1-.2)
+1-(1-.5)*(1-.2)*(1-.15)
+1-(1-.5)*(1-.6)*(1-.15)
 
 
