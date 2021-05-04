23d188cde784c0a4cf8af4b2c689beff98d97629
Kelsey Sumner <sumner.kelsey@gmail.com>
2021-05-04 11:59:05 -0400

    updating script

--- Haplotype_calling_tutorial/step1_split_reads_slurm.sh
+++ Haplotype_calling_tutorial/step1_split_reads_slurm.sh
@@ -1,4 +1,4 @@
-#!/bin/tcsh
+#!/bin/bash
 #
 ##SBATCH -p common                # Partition to submit to (comma separated)
 #SBATCH -J splitRef1         # Job name
@@ -14,6 +14,9 @@ module load cutadapt/1.8.3-gcb01
 module load fastqc/0.11.5-fasrc01
 module load java/1.7.0_60-fasrc01
 module load jdk/9-gcb01
+
+path_bbmap=/data/taylorlab/software/bbmap
+export PATH=$path_bbmap:$PATH
 
 ./step1_splitSyncReadsMultiRef.pl 2 /data/taylorlab/kms94/haplotype_tutorial/refs/AMA/AMA.fasta,/data/taylorlab/kms94/haplotype_tutorial/refs/CSP/CSP.fasta /data/taylorlab/kms94/haplotype_tutorial/out /data/taylorlab/kms94/haplotype_tutorial/raw_reads/1 /data/taylorlab/kms94/haplotype_tutorial/raw_reads/2 /data/taylorlab/kms94/haplotype_tutorial/adapters/forwardPrimers.fasta /data/taylorlab/kms94/haplotype_tutorial/adapters/reversePrimers.fasta
 
